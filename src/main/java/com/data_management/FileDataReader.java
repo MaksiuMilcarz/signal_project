b35ca45462d354e328289ed5485b1a31589a2ed4
MaksiuMilcarz <141926445+MaksiuMilcarz@users.noreply.github.com>
2024-05-02 09:29:41 +0200

    added output files

---
+++ src/main/java/com/data_management/FileDataReader.java
@@ -0,0 +1,14 @@
+package com.data_management;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class FileDataReader implements DataReader{
+    public void readData(DataStorage dataStorage) throws IOException {
+        // Read data from a file and store it in the data storage
+        File directory = new File("/Users/maksiuuuuuuu/Desktop/SoftwareEngineering/project/signal_project/output");
+        for (Path file : ) {
+            // Read data from the file
+        }
+    }
+}
