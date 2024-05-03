d851ee4c3b7c8a11ef8d8bb3db8c90cedf9debb1
MaksiuMilcarz <141926445+MaksiuMilcarz@users.noreply.github.com>
2024-05-03 22:16:06 +0200

    Corrected file Reader file + plugin to allow debug

--- src/main/java/com/data_management/DataStorage.java
+++ src/main/java/com/data_management/DataStorage.java
@@ -1,6 +1,5 @@
 package com.data_management;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/main/java/com/data_management/FileDataReader.java
+++ src/main/java/com/data_management/FileDataReader.java
@@ -4,11 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import com.data_management.DataReader;
-import com.data_management.DataStorage;
 
 public class FileDataReader implements DataReader{
     public void readData(DataStorage dataStorage) throws IOException {
@@ -17,23 +15,24 @@ public class FileDataReader implements DataReader{
         File[] files = directory.listFiles();
         for (File file : files) {
             if (file.isFile() && !file.getName().equals("Alert.txt")) {
+                Pattern pattern = Pattern.compile("Patient ID: (\\d+), Timestamp: (\\d+), Label: (.*), Data: (\\d+\\.\\d+)");
                 try (FileReader fr = new FileReader(file); BufferedReader reader = new BufferedReader(fr)){
-                    Pattern pattern = Pattern.compile("Patient ID: (\\d+), Timestamp: (\\d+), Label: (.*), Data: (\\d+\\.\\d+)");
-                    Matcher matcher = pattern.matcher(reader.readLine());
-                
-                    while (matcher.find()) {
-                        int patientId = Integer.parseInt(matcher.group(1));
-                        long timestamp = Long.parseLong(matcher.group(2));
-                        String recordType = matcher.group(3);
-                        double measurementValue;
-                        if(file.getName().equals("Saturation.txt")){
-                            // Saturation data is in percentage so we need to get rid of the percentage sign
-                            measurementValue = Double.parseDouble(matcher.group(4).replace("%", ""));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        Matcher matcher = pattern.matcher(line);
+                        while (matcher.find()) {
+                            int patientId = Integer.parseInt(matcher.group(1));
+                            long timestamp = Long.parseLong(matcher.group(2));
+                            String recordType = matcher.group(3);
+                            double measurementValue;
+                            if (file.getName().equals("Saturation.txt")) {
+                                // Saturation data is in percentage so we need to get rid of the percentage sign
+                                measurementValue = Double.parseDouble(matcher.group(4).replace("%", ""));
+                            } else {
+                                measurementValue = Double.parseDouble(matcher.group(4));
+                            }
+                            dataStorage.addPatientData(patientId, measurementValue, recordType, timestamp);
                         }
-                        else{
-                            measurementValue = Double.parseDouble(matcher.group(4));
-                        }
-                        dataStorage.addPatientData(patientId, measurementValue, recordType, timestamp);
                     }
                 }
                 catch(IOException e){
@@ -52,6 +51,13 @@ public class FileDataReader implements DataReader{
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(storage.getAllPatients().size());
+        List<PatientRecord> records = storage.getRecords(100, 1700000000000L, 1800000000000L);
+        for (PatientRecord record : records) {
+            System.out.println("Record for Patient ID: " + record.getPatientId() +
+                    ", Type: " + record.getRecordType() +
+                    ", Data: " + record.getMeasurementValue() +
+                    ", Timestamp: " + record.getTimestamp());
+        }
+        
     }
 }
